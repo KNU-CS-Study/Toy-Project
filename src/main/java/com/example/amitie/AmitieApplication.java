1057678bd799c38f8f7ba049a2e91257859a0e7f
Dong-wook94 <gygacpu@naver.com>
2020-05-18 21:50:59 +0900

    test deploy

--- src/main/java/com/example/amitie/AmitieApplication.java
+++ src/main/java/com/example/amitie/AmitieApplication.java
@@ -2,10 +2,16 @@ package com.example.amitie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AmitieApplication {
+public class AmitieApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(AmitieApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(AmitieApplication.class, args);
     }
--- src/main/java/com/example/amitie/controller/HelloController.java
+++ src/main/java/com/example/amitie/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping
+    @RequestMapping("/")
     public String hello(){
         return "Hello World! test hi";
     }
